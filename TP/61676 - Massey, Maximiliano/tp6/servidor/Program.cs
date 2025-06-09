3e47bf18a226e045790f4e0ee7e2f72d2666f510
MasseyMaxi <MaxiMassey5@gmail.com>
2025-06-09 00:24:08 -0300

    Massey Maximiliano-61676
    entidades establecidas y se agregaron las carpetas datos y modelosdd

--- TP/61676 - Massey, Maximiliano/tp6/servidor/Program.cs
+++ TP/61676 - Massey, Maximiliano/tp6/servidor/Program.cs
@@ -1,21 +1,33 @@
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Agregar servicios CORS para permitir solicitudes desde el cliente
 builder.Services.AddCors(options => {
     options.AddPolicy("AllowClientApp", policy => {
         policy.WithOrigins("http://localhost:5177", "https://localhost:7221")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+            .AllowAnyHeader()
+            .AllowAnyMethod();
     });
 });
 
 // Agregar controladores si es necesario
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<ContenidoTiendaDb>(options => {
+    options.UseSqlite("Data Source=tienda.db");
+});
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<ContenidoTiendaDb>();
+    dbContext.Database.EnsureCreated(); // Asegurarse de que la base de datos esté actualizada
+}
+
 // Configurar el pipeline de solicitudes HTTP
-if (app.Environment.IsDevelopment()) {
+if (app.Environment.IsDevelopment())
+{
     app.UseDeveloperExceptionPage();
 }
 
