475d11c42718e1c717047c0b0153f9e1d7b34350
lourdesaguirre97 <martina.araoz39@gmail.com>
2025-06-27 14:06:53 -0300

    tp6-61252 (#742)

--- TP/61252 - Aguirre Rojas, Lourdes Valentina/tp6/cliente/Program.cs
+++ TP/61252 - Aguirre Rojas, Lourdes Valentina/tp6/cliente/Program.cs
@@ -7,10 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Configurar el HttpClient para apuntar al servidor API
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5184") });
-
-// Registrar el servicio API
-builder.Services.AddScoped<ApiService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5100") });
+builder.Services.AddScoped<TiendaService>();
 
 await builder.Build().RunAsync();
